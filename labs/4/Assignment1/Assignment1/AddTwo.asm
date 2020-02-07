62758d0c5f8894237d12d804ae2ee482b793e545
Alexandre Geraldo <ageraldo@itktechnologies.com>
2020-02-07 09:27:50 -0500

    exercise 1

--- labs/4/Assignment1/Assignment1/AddTwo.asm
+++ labs/4/Assignment1/Assignment1/AddTwo.asm
@@ -1,5 +1,6 @@
 ; Lab 4 - Assignment 1
 ; opcode <destination> <source>
+; unsigned operation
 
 .386
 .model flat,stdcall
@@ -13,9 +14,9 @@ main proc
 	mov ecx,0h		; debug purpose only
 	mov edx,0h		; debug purpose only
 	
-	mov	bl, 94d		; 8 bits  (0x5E)
-	mov cl, 64d		; 8 bits  (0x40)
-	mov dl, 31d		; 8 bits  (0X1F)
+    mov bl, 94d     ; 8 bits  (0x5E)
+	mov cl, 64d     ; 8 bits  (0x40)
+	mov dl, 31d     ; 8 bits  (0X1F)
 
 	add cl, bl		; 94  + 64 = 158 (0x9E)
 	;==========================================
@@ -24,11 +25,13 @@ main proc
 	;     ----       ------------
 	;     158          1001 1110
 	;   
-	;     EFLASG
+	;     EFLAGS
 	;			OV (overflow) = 1 
 	;			PL (signal)   = 1 
 	;			ZR (zero)     = 0 
-	;			CY (carry)    = 0 
+	;			CY (carry)    = 0
+	;
+	; VS Output : OV = 1 UP = 0 EI = 1 PL = 1 ZR = 0 AC = 0 PE = 0 CY = 0 
 	;==========================================
 
 	sub cl, dl		; 158 - 31 = 127 (0x7F)
@@ -38,14 +41,13 @@ main proc
 	;     ----       ------------
 	;     127        1|0111 1111
 	;                   
-	;     EFLASG
+	;     EFLAGS
 	;			OV (overflow) = 1 
 	;			PL (signal)   = 0 
 	;			ZR (zero)     = 0 
 	;			CY (carry)    = 1  ?? (VS CY = 0) 
 	;
 	; VS Output : OV = 1 UP = 0 EI = 1 PL = 0 ZR = 0 AC = 1 PE = 0 CY = 0 
-	;
 	;==========================================
 
 	mov al, cl      ; 127 = (0x7F)
