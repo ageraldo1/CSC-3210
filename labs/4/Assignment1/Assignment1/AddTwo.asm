0735c84275a988af3c8e00070d93ed4fa91e0a10
Alexandre Geraldo <ageraldo@itktechnologies.com>
2020-02-07 08:16:48 -0500

    assigment 1 - added comments

--- labs/4/Assignment1/Assignment1/AddTwo.asm
+++ labs/4/Assignment1/Assignment1/AddTwo.asm
@@ -17,18 +17,39 @@ main proc
 	mov cl, 64d		; 8 bits  (0x40)
 	mov dl, 31d		; 8 bits  (0X1F)
 
-	add cl, bl		; 94  + 64 = 158 (0x9E)	
+	add cl, bl		; 94  + 64 = 158 (0x9E)
+	;==========================================
+	;      94   =      0101 1110
+	;    + 64   =  +   0100 0000
+	;     ----       ------------
+	;     158          1001 1110
+	;   
+	;     EFLASG
+	;			OV (overflow) = 1 
+	;			PL (signal)   = 1 
+	;			ZR (zero)     = 0 
+	;			CY (carry)    = 0 
+	;==========================================
+
 	sub cl, dl		; 158 - 31 = 127 (0x7F)
+	;==========================================
+	;     158   =      1001 1110
+	;    - 31   =  +   1110 0001
+	;     ----       ------------
+	;     127        1|0111 1111
+	;                   
+	;     EFLASG
+	;			OV (overflow) = 1 
+	;			PL (signal)   = 0 
+	;			ZR (zero)     = 0 
+	;			CY (carry)    = 1  ?? (VS CY = 0) 
+	;
+	; VS Output : OV = 1 UP = 0 EI = 1 PL = 0 ZR = 0 AC = 1 PE = 0 CY = 0 
+	;
+	;==========================================
 
 	mov al, cl      ; 127 = (0x7F)
 
-	COMMENT !
-	    CFLAGS : 
-			OV (overflow) = 1 
-			PL (signal)   = 0 
-			ZR (zero)     = 0 
-			CY (carry)    = 0 
-	!
 
 	invoke ExitProcess,0
 main endp
